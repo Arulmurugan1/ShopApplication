ad12c2659b578a471a8f2fb280255bf599fb6fc6
Arulmurugan C <arulmurugantechie@gmail.com>
2026-05-24 15:59:10 +0530

    stage 2 deployment

--- Backend/SpringBoot/order-service/src/main/java/com/shop/service/tech/order_service/repository/IProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shop.service.tech.order_service.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.shop.service.tech.order_service.dao.Product;
-
-public interface IProductRepository extends MongoRepository<Product, String> {
-    
-}
